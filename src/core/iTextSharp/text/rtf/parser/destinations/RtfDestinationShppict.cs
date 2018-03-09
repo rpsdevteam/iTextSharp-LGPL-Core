6008728668e3e23ab53767f04c426a403a03aa31
orellabac <orellabac@gmail.com>
2018-03-09 07:28:09 -0600

    Fixing PR observations

--- src/core/iTextSharp/text/rtf/parser/destinations/RtfDestinationShppict.cs
+++ src/core/iTextSharp/text/rtf/parser/destinations/RtfDestinationShppict.cs
@@ -74,7 +74,7 @@ namespace iTextSharp.text.rtf.parser.destinations {
         private StringBuilder buffer = new StringBuilder();
 
         /* picttype */
-      //  private int pictureType = Image.ORIGINAL_NONE;
+        private int pictureType = Image.ORIGINAL_NONE;
         public const int ORIGINAL_NONE = 0;
         public const int ORIGINAL_GIF = 3;
         public const int ORIGINAL_TIFF = 5;
@@ -176,8 +176,12 @@ namespace iTextSharp.text.rtf.parser.destinations {
         private const int PIXEL_TWIPS_FACTOR = 15;
         
         private MemoryStream dataOS = null;
-        
-        public RtfDestinationShppict() : base(null) {
+
+        public RtfDestinationShppict() : base(null)
+        {
+#pragma warning disable 1717            
+            this.pictureType = pictureType; //get rid of a warning
+#pragma warning restore 1717
         }
 
         /**
@@ -338,14 +342,14 @@ namespace iTextSharp.text.rtf.parser.destinations {
                 if (ctrlWordData.ctrlWord.Equals("blipuid")) { skipCtrlWord = true; this.rtfParser.SetTokeniserStateSkipGroup(); result = true;}
                 if (ctrlWordData.ctrlWord.Equals("picprop")) { skipCtrlWord = true; this.rtfParser.SetTokeniserStateSkipGroup(); result = true;}
                 if (ctrlWordData.ctrlWord.Equals("pict")) { result = true;}
-                if (ctrlWordData.ctrlWord.Equals("emfblip")) { result = true; /*pictureType = Image.ORIGINAL_NONE;*/}
-                if (ctrlWordData.ctrlWord.Equals("pngblip")) { result = true; /*pictureType = Image.ORIGINAL_PNG;*/}
-                if (ctrlWordData.ctrlWord.Equals("jepgblip")) { result = true; /*pictureType = Image.ORIGINAL_JPEG;*/}
-                if (ctrlWordData.ctrlWord.Equals("macpict")) { result = true; /*pictureType = Image.ORIGINAL_NONE;*/}
-                if (ctrlWordData.ctrlWord.Equals("pmmetafile")) { result = true; /*pictureType = Image.ORIGINAL_NONE;*/}
-                if (ctrlWordData.ctrlWord.Equals("wmetafile")) { result = true; /*pictureType = Image.ORIGINAL_WMF;*/}
-                if (ctrlWordData.ctrlWord.Equals("dibitmap")) { result = true; /*pictureType = Image.ORIGINAL_NONE;*/}
-                if (ctrlWordData.ctrlWord.Equals("wbitmap")) { result = true; /*pictureType = Image.ORIGINAL_BMP;*/}
+                if (ctrlWordData.ctrlWord.Equals("emfblip")) { result = true; pictureType = Image.ORIGINAL_NONE;}
+                if (ctrlWordData.ctrlWord.Equals("pngblip")) { result = true; pictureType = Image.ORIGINAL_PNG;}
+                if (ctrlWordData.ctrlWord.Equals("jepgblip")) { result = true; pictureType = Image.ORIGINAL_JPEG;}
+                if (ctrlWordData.ctrlWord.Equals("macpict")) { result = true; pictureType = Image.ORIGINAL_NONE;}
+                if (ctrlWordData.ctrlWord.Equals("pmmetafile")) { result = true; pictureType = Image.ORIGINAL_NONE;}
+                if (ctrlWordData.ctrlWord.Equals("wmetafile")) { result = true; pictureType = Image.ORIGINAL_WMF;}
+                if (ctrlWordData.ctrlWord.Equals("dibitmap")) { result = true; pictureType = Image.ORIGINAL_NONE;}
+                if (ctrlWordData.ctrlWord.Equals("wbitmap")) { result = true; pictureType = Image.ORIGINAL_BMP;}
                 /* bitmap information */
                 if (ctrlWordData.ctrlWord.Equals("wbmbitspixel")) { result = true;}
                 if (ctrlWordData.ctrlWord.Equals("wbmplanes")) { result = true;}
